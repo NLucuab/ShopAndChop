9c9c1f1cdc97da3b717ad437ee3c8bb45f74bf53
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-08-08 13:15:41 -0700

    Adds Controllers and changes storyboard colors

---
+++ ShopAndChop_Final/Controllers/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  ShopAndChop_Final
+//
+//  Created by Nyckolle Grace T. Lucuab on 8/8/21.
+//
+
+import SwiftUI
+
+struct RegisterViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterViewController()
+    }
+}
---
+++ ShopAndChop_Final/Controllers/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  ShopAndChop_Final
+//
+//  Created by Nyckolle Grace T. Lucuab on 8/8/21.
+//
+
+import SwiftUI
+
+struct WelcomeViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeViewController()
+    }
+}
