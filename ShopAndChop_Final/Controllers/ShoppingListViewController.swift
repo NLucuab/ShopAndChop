aa22af24f30e562d3d62902acce463ea6bcb8629
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-08-11 00:18:38 -0700

    deletes unneeded icons and implements delete functionality for shopping list

--- ShopAndChop_Final/Controllers/ShoppingListViewController.swift
+++ ShopAndChop_Final/Controllers/ShoppingListViewController.swift
@@ -20,6 +20,7 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
         title = "Shopping List"
         view.addSubview(table)
         table.dataSource = self
@@ -28,7 +29,7 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource {
         }
     
     @objc private func didTapAdd() {
-        let alert = UIAlertController(title: "New Item", message: "Please enter a new grocery list item!", preferredStyle: .alert)
+        let alert = UIAlertController(title: "New Item", message: "Please enter a new shopping list item!", preferredStyle: .alert)
         
         alert.addTextField { field in
             field.placeholder = "Enter item..."
@@ -39,6 +40,9 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource {
                 if let text = field.text, !text.isEmpty {
                     // Enter new shopping list item
                     DispatchQueue.main.async {
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItems.append(text)
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
                         self.items.append(text)
                         self.table.reloadData()
                     }
@@ -65,10 +69,34 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource {
         cell.textLabel?.text = items[indexPath.row]
         return cell
     }
+    
+//    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+//        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { _, indexPath in
+//            self.items.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        }
+//        return [deleteAction]
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
+    }
+    
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            tableView.beginUpdates()
+//            items.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//            tableView.endUpdates()
+//        }
+//    }
 
     
     }
     
+
 
 
 
