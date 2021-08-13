cd7d444255793f30a8df02f922ef47db635afa05
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-08-13 13:05:31 -0700

    adds collection view and icons

--- ShopAndChop_Final/Controllers/HomeViewController.swift
+++ ShopAndChop_Final/Controllers/HomeViewController.swift
@@ -2,13 +2,42 @@
 //  HomeViewController.swift
 //  ShopAndChop_Final
 //
-//  Created by Nyckolle Grace T. Lucuab on 8/9/21.
+//  Created by Nyckolle Grace T. Lucuab on 8/13/21.
 //
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CategoryCollectionViewCell
+        cell?.imageView.image = UIImage(named: imageArray[indexPath.row])
+        cell?.Label.text = imageArray[indexPath.row]
+        
+        return cell!
+    }
+    
 
+    
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    
+    var imageArray = ["Beverages",
+                      "Bakery",
+                      "Canned Goods",
+                      "Dairy",
+                      "Dry Goods",
+                      "Frozen Foods",
+                      "Meat",
+                      "Produce",
+                      "Cleaning Products",
+                      "Paper Products",
+                      "Personal Care",
+                      "Other"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,3 +47,22 @@ class HomeViewController: UIViewController {
 
 
 }
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = collectionView.bounds
+        return CGSize(width: bounds.width/2, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
